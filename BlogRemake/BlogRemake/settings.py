0d0489b54841521451e659d6d2f99a2e8ebf9c6d
Your Name <sjonesmusician@gmail.com>
2019-05-03 10:59:42 -0400

    added files for heroku

--- BlogRemake/BlogRemake/settings.py
+++ BlogRemake/BlogRemake/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# samjones-djangoblog.com/BlogApp/static'
 
 LOGIN_REDIRECT_URL = '/'
